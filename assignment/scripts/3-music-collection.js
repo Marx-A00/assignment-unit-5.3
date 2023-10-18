26494ea06ecfb8f145a267fe6545422ba9cb8721
Marcos Andrade <mnandrade1999@gmail.com>
2023-10-18 15:39:22 -0500

    completed up until stretch goals

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,53 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+
+function addToCollection(collection,title,artist,yearPublished){
+  let newRecord = {
+    collection: collection,
+    title : title,
+    artist: artist,
+    yearPublished: yearPublished 
+  };
+  collection.push(newRecord);
+
+  return newRecord;
+}
+addToCollection(myCollection,"Die Lit","Playboi Carti",2018);
+addToCollection(myCollection,"Icedancer","Bladee",2018);
+addToCollection(myCollection,"Yeezus","Kanye West",2013);
+addToCollection(myCollection,"Good Luck","Bladee",2020);
+addToCollection(myCollection,"James Blake","James Blake",2011);
+addToCollection(myCollection,"After Hours","The Weeknd",2020);
+
+function showCollection(collection){
+  for(let record of collection){
+    console.log(record.title + " by " +  record.artist + " Published in " + record.yearPublished);
+  }
+}
+
+showCollection(myCollection);
+
+function findByArtist(collection,artist){
+  let matchingResults = [];
+  for(let record of collection){
+    if(record.artist == artist){
+      matchingResults.push(record);
+    }
+  }
+  return matchingResults;
+}
+
+console.log(findByArtist(myCollection,"Bladee"));
+
+
+function search(collection,searchCriteria){
+
+}
+
+
+
 
 
 
